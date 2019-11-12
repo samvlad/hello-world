ec24b23bfbdc6565a1da12183b0c022abce4bd8a
samvlad <samvla2010@gmail.com>
2019-11-12 13:58:04 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+print("Hello World\n")
+#user_name = input('What is your name?')
+#print('\tHi, ' + user_name.title())
+
+monthes = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+print(monthes[0].title())
